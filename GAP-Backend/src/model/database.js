2fbb4a65be52f10f5eef1d79d499f09aecfaa046
Nicolas <ncontreras@colegium.com>
2020-07-07 23:03:56 -0400

    database

--- GAP-Backend/src/model/database.js
+++ GAP-Backend/src/model/database.js
@@ -1,10 +1,10 @@
 var Sequelize = require('sequelize');
 const sequelize = new Sequelize(
-    'evaluaciongap', //database
-    'root', // user
-    '', //password
+    'heroku_08a6962b9fef948', //database
+    'b5f115ad770aaf', // user
+    '5a8daac0', //password
     {
-        host: 'localhost',
+        host: 'us-cdbr-east-02.cleardb.com',
         dialect: 'mysql'
     }
 );
